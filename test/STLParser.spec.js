945e5b50d786cff2f9ae1211a727bb18eb1f0918
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-08-23 17:10:55 +0200

    fix(tests): updated tests to correctly match those on master

--- test/STLParser.spec.js
+++ test/STLParser.spec.js
@@ -14,29 +14,28 @@ test.cb('STL parser tests: can parse ascii stl files', t => {
   let data = fs.readFileSync('./data/slotted_disk_ascii.stl', 'binary')
   let stlObs = parse(data) // we get an observable back
 
-    stlObs.forEach(function (parsedSTL) {
-      assert.equal(parsedSTL.positions.length / 3, 864) // we divide by three because each entry is 3 long
-      done()
-    })
+  stlObs.forEach(function (parsedSTL) {
+    t.deepEqual(parsedSTL.positions.length / 3, 864) // we divide by three because each entry is 3 long
+    t.end()
   })
+})
 
-  it('can parse binary stl files', done => {
-    let data = fs.readFileSync('test/data/pr2_head_pan_bin.stl', 'binary')
-    let stlObs = parse(data) // we get an observable back
+test.cb('STL parser tests: can parse binary stl files', t => {
+  let data = fs.readFileSync('./data/pr2_head_pan_bin.stl', 'binary')
+  let stlObs = parse(data) // we get an observable back
 
-    stlObs.forEach(function (parsedSTL) {
-      assert.equal(parsedSTL.positions.length / 3, 3000) // we divide by three because each entry is 3 long
-      done()
-    })
+  stlObs.forEach(function (parsedSTL) {
+    t.deepEqual(parsedSTL.positions.length / 3, 3000) // we divide by three because each entry is 3 long
+    t.end()
   })
+})
 
 test.cb('STL parser tests: should handle errors gracefully', t => {
   let data = {foo: '42'}
   let stlObs = parse(data) // we get an observable back
 
-    stlObs.forEach(undefined, function (error) {
-      assert.equal(error.message, 'First argument to DataView constructor must be an ArrayBuffer')
-      done()
-    })
+  stlObs.forEach(undefined, function (error) {
+    t.deepEqual(error.message, 'First argument to DataView constructor must be an ArrayBuffer')
+    t.end()
   })
 })
