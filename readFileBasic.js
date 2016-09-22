febff3688327624b6b3e4af60fdd9531b041ab7c
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-09-22 14:53:50 +0200

    feat(basic file Reader): added "brute force file reader"

---
+++ readFileBasic.js
@@ -0,0 +1,13 @@
+export default function readFileBasic (file) {
+
+  return new Promise(function (resolve, reject) {
+    const reader = new FileReader()
+
+    reader.onload = function (openFile) {
+      const fileData = openFile.target.result
+      console.log('openFile', openFile.target.result)
+      resolve(fileData)
+    }
+    reader.readAsArrayBuffer(file)
+  })
+}
