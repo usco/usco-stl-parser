d617d0444bb9fb42007f6afbe0da4a2ec00fff95
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-09-22 14:54:15 +0200

    chore(main): added a bit of boilerplate code, import of new modules

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 import readAsStream from './readAsStream'
+import readFileBasic from './readFileBasic'
+import workerTest from './workerTest'
+
+
+function repeat (times, fn, params) {
+  for (var i = 0; i < times; i++) {
+    fn(params)
+  }
+}
 
 function handleFileSelect (e) {
   e.stopPropagation()
   e.preventDefault()
 
-  var files = e.dataTransfer.files // FileList object.
-
   // files is a FileList of File objects. List some properties.
-  let _files = []
-  for (var i = 0, f; f = files[i]; i++){
-    _files.push(f)
+  let files = []
+  for (var i = 0, f; f = e.dataTransfer.files[i]; i++) {
+    files.push(f)
   }
-  let output = _files.map(function(f){
+  let output = files.map(function (f) {
     return `<li>
       <strong> ${escape(f.name)} </strong> (${f.type || 'n/a' }) - ${f.size} bytes,
       last modified: ${f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a'}
       </li>`
   })
 
-  console.log('output', output)
   document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>'
 
-
-  readAsStream(_files[0])
+  const testCount = 10
+  //repeat(testCount, workerTest, files[0])
+  readFileBasic(files[0]).then(e=>console.log('fileData', e))
+// readAsStream(files[0])
 }
 
 function handleDragOver (e) {
