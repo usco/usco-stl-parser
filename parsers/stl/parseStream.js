9295bc37a0308a3e11082153f82967e91598b4ad
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-09-28 11:38:16 +0200

    feat(stlStreamParser): still wip: continued worked on streaming stl parser: - correctly computing remaining data for 'next loop' based on chunks size - buffer concatenation based on previous remains + current buffer - lot of minor fixes & improvements

--- parsers/stl/parseStream.js
+++ parsers/stl/parseStream.js
@@ -1,6 +1,6 @@
 import { isDataBinary, ensureBinary, ensureString } from './utils'
 
-//import through2 from 'through2'
+// import through2 from 'through2'
 var through2 = require('through2')
 
 export default function makeStlStreamParser () {
@@ -12,60 +12,88 @@ export default function makeStlStreamParser () {
   let positions
   let normals
 
-  const dataOffset = 84
+  const dataStartOffset = 84
   const faceLength = 12 * 4 + 2
+  const faceLengthByte = faceLength * 8
+
   let offset = 0
   let face = 0
 
-  let fakeAccumulatedData = []
+  let faceOffset = 0
 
+  let previousRemainderDataLength = 0
+  let previousRemainderData
 
-  const parser =  function (chunk, enc, callback) {
-    /*for (var i = 0; i < chunk.length; i++)
-      if (chunk[i] === 97)
-        chunk[i] = 122 // swap 'a' for 'z'*/
-    console.log('chunk', chunk.length, chunkNb)
-    chunk = ensureBinary(chunk)
-    const data = chunk.buffer
+  const parser = function (chunk, enc, callback) {
+    //console.log('chunk', chunk.length, chunkNb)
+    let workChunk = previousRemainderData ? Buffer.concat([previousRemainderData, chunk]): chunk
+    workChunk = ensureBinary(workChunk)
+    const data = workChunk.buffer
     const reader = new DataView(data)
 
-    let remainingDataInChunk = 0
+    let remainingDataInChunk = workChunk.length
 
     if (chunkNb === 0) {
       isBinary = isDataBinary(data)
 
       faces = reader.getUint32(80, true)
-      //FIXME meh , we are allocating data for the whole thing: not great
-      //positions = new Float32Array(faces * 3 * 3)
-      //normals = new Float32Array(faces * 3 * 3)
-      console.log('faces', faces)
-      remainingDataInChunk = chunk.length - dataOffset
+      // FIXME meh , we are allocating data for the whole thing: not great
+      positions = new Float32Array(faces * 3 * 3)
+      normals = new Float32Array(faces * 3 * 3)
+      console.log('faces', faces, 'chunk size', chunk.length, 'workChunk', workChunk.length)
+      remainingDataInChunk = workChunk.length - dataStartOffset
     }
 
-    //start = dataOffset + face * faceLength
-    //vertexstart = start + i * 12
+    // process all faces data that we can
+    // offset face => faces handled so far / faceLength
+
+    let facesInChunk = Math.floor(remainingDataInChunk / faceLength)
+    let remainderDataLength = remainingDataInChunk % faceLength
+
+    for (face = faceOffset; face < facesInChunk + faceOffset; face += 1) {
+      /*let start = dataStartOffset + face * faceLength
+      for (var i = 1; i <= 3; i++) {
+        let vertexstart = start + i * 12
+        positions[ offset     ] = reader.getFloat32(vertexstart, true)
+        positions[ offset + 1 ] = reader.getFloat32(vertexstart + 4, true)
+        positions[ offset + 2 ] = reader.getFloat32(vertexstart + 8, true)
+
+        normals[offset] = reader.getFloat32(start, true)
+        normals[offset + 1] = reader.getFloat32(start + 4, true)
+        normals[offset + 2] = reader.getFloat32(start + 8, true)
+        offset += 3
+      }*/
+    }
+    console.log('faceOffset',faceOffset,'facesInChunk', facesInChunk, 'remainderDataLength', remainderDataLength, 'current face ', face)
 
     // update size
     chunkNb += 1
     totalSize += chunk.length
 
-    fakeAccumulatedData.push(chunkNb)
+    previousRemainderDataLength = remainderDataLength
+    previousRemainderData = workChunk.slice(workChunk.length - remainderDataLength)
+    faceOffset = face
 
 
-    callback(null, `${fakeAccumulatedData}`)
+    console.log('previousRemainderData', previousRemainderData)
+
+
+    callback()
+
+  // callback(null, `${fakeAccumulatedData}`)
   }
 
   return through2(parser)
 
-  // data = ensureBinary(data)
-  // const isBinary = isDataBinary(data)
-  // return isBinary ? parseBinary(data) : parseASCII(ensureString(data))
+// data = ensureBinary(data)
+// const isBinary = isDataBinary(data)
+// return isBinary ? parseBinary(data) : parseASCII(ensureString(data))
 }
 
 export function parseBinary (data) {
   var reader = new DataView(data)
   var faces = reader.getUint32(80, true)
-  var dataOffset = 84
+  var dataStartOffset = 84
   var faceLength = 12 * 4 + 2
   var offset = 0
 
@@ -73,14 +101,14 @@ export function parseBinary (data) {
   var normals = new Float32Array(faces * 3 * 3)
 
   for (var face = 0; face < faces; face++) {
-    var start = dataOffset + face * faceLength
+    var start = dataStartOffset + face * faceLength
 
     for (var i = 1; i <= 3; i++) {
       var vertexstart = start + i * 12
 
-      positions[ offset     ] = reader.getFloat32( vertexstart, true )
-      positions[ offset + 1 ] = reader.getFloat32( vertexstart + 4, true )
-      positions[ offset + 2 ] = reader.getFloat32( vertexstart + 8, true )
+      positions[ offset     ] = reader.getFloat32(vertexstart, true)
+      positions[ offset + 1 ] = reader.getFloat32(vertexstart + 4, true)
+      positions[ offset + 2 ] = reader.getFloat32(vertexstart + 8, true)
 
       normals[offset] = reader.getFloat32(start, true)
       normals[offset + 1] = reader.getFloat32(start + 4, true)
