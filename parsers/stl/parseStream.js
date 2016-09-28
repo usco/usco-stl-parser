af3f2e295c91978b4b1d1644cc7d2aef87ef6533
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-09-28 13:40:23 +0200

    feat(parseStlStream): basics of streaming stl parser SHOULD be ok - correcly reading input data and generating "chunks" of output data - cleaned up internalls

--- parsers/stl/parseStream.js
+++ parsers/stl/parseStream.js
@@ -16,7 +16,7 @@ export default function makeStlStreamParser () {
   const faceLength = 12 * 4 + 2
   const faceLengthByte = faceLength * 8
 
-  let offset = 0
+  // let offset = 0
   let face = 0
 
   let faceOffset = 0
@@ -25,23 +25,27 @@ export default function makeStlStreamParser () {
   let previousRemainderData
 
   const parser = function (chunk, enc, callback) {
-    //console.log('chunk', chunk.length, chunkNb)
-    let workChunk = previousRemainderData ? Buffer.concat([previousRemainderData, chunk]): chunk
+    // console.log('chunk', chunk.length, chunkNb)
+    let workChunk = previousRemainderData ? Buffer.concat([previousRemainderData, chunk]) : chunk
     workChunk = ensureBinary(workChunk)
     const data = workChunk.buffer
     const reader = new DataView(data)
 
     let remainingDataInChunk = workChunk.length
+    let startOffset = 0
 
     if (chunkNb === 0) {
       isBinary = isDataBinary(data)
 
       faces = reader.getUint32(80, true)
-      // FIXME meh , we are allocating data for the whole thing: not great
-      positions = new Float32Array(faces * 3 * 3)
-      normals = new Float32Array(faces * 3 * 3)
+
+      // FIXME meh , we are allocating data for the whole thing: not great, except in 'accumulator' mode
+      // positions = new Float32Array(faces * 3 * 3)
+      // normals = new Float32Array(faces * 3 * 3)
       console.log('faces', faces, 'chunk size', chunk.length, 'workChunk', workChunk.length)
       remainingDataInChunk = workChunk.length - dataStartOffset
+
+      startOffset = dataStartOffset
     }
 
     // process all faces data that we can
@@ -50,21 +54,28 @@ export default function makeStlStreamParser () {
     let facesInChunk = Math.floor(remainingDataInChunk / faceLength)
     let remainderDataLength = remainingDataInChunk % faceLength
 
+    positions = new Float32Array(facesInChunk * 3 * 3)
+    normals = new Float32Array(facesInChunk * 3 * 3)
+
+    let lface = 0
+    let offset = 0
+
     for (face = faceOffset; face < facesInChunk + faceOffset; face += 1) {
-      /*let start = dataStartOffset + face * faceLength
+      let start = startOffset + lface * faceLength
       for (var i = 1; i <= 3; i++) {
         let vertexstart = start + i * 12
-        positions[ offset     ] = reader.getFloat32(vertexstart, true)
+        positions[ offset ] = reader.getFloat32(vertexstart, true)
         positions[ offset + 1 ] = reader.getFloat32(vertexstart + 4, true)
         positions[ offset + 2 ] = reader.getFloat32(vertexstart + 8, true)
 
-        normals[offset] = reader.getFloat32(start, true)
-        normals[offset + 1] = reader.getFloat32(start + 4, true)
-        normals[offset + 2] = reader.getFloat32(start + 8, true)
+        normals[ offset ] = reader.getFloat32(start, true)
+        normals[ offset + 1 ] = reader.getFloat32(start + 4, true)
+        normals[ offset + 2 ] = reader.getFloat32(start + 8, true)
         offset += 3
-      }*/
+      }
+      lface += 1
     }
-    console.log('faceOffset',faceOffset,'facesInChunk', facesInChunk, 'remainderDataLength', remainderDataLength, 'current face ', face)
+    console.log('faceOffset', faceOffset, 'facesInChunk', facesInChunk, 'remainderDataLength', remainderDataLength, 'current face ', face)
 
     // update size
     chunkNb += 1
@@ -74,13 +85,13 @@ export default function makeStlStreamParser () {
     previousRemainderData = workChunk.slice(workChunk.length - remainderDataLength)
     faceOffset = face
 
-
     console.log('previousRemainderData', previousRemainderData)
 
-
-    callback()
-
-  // callback(null, `${fakeAccumulatedData}`)
+    // we can only send a single buffer out , so concat the two
+    const positionsBuffer = new Buffer(positions)
+    const normalsBuffer = new Buffer(normals)
+    const result = Buffer.concat([positionsBuffer, normalsBuffer]) // [positions.buffer, normals.buffer])
+    callback(null, result)
   }
 
   return through2(parser)
@@ -88,33 +99,4 @@ export default function makeStlStreamParser () {
 // data = ensureBinary(data)
 // const isBinary = isDataBinary(data)
 // return isBinary ? parseBinary(data) : parseASCII(ensureString(data))
-}
-
-export function parseBinary (data) {
-  var reader = new DataView(data)
-  var faces = reader.getUint32(80, true)
-  var dataStartOffset = 84
-  var faceLength = 12 * 4 + 2
-  var offset = 0
-
-  var positions = new Float32Array(faces * 3 * 3)
-  var normals = new Float32Array(faces * 3 * 3)
-
-  for (var face = 0; face < faces; face++) {
-    var start = dataStartOffset + face * faceLength
-
-    for (var i = 1; i <= 3; i++) {
-      var vertexstart = start + i * 12
-
-      positions[ offset     ] = reader.getFloat32(vertexstart, true)
-      positions[ offset + 1 ] = reader.getFloat32(vertexstart + 4, true)
-      positions[ offset + 2 ] = reader.getFloat32(vertexstart + 8, true)
-
-      normals[offset] = reader.getFloat32(start, true)
-      normals[offset + 1] = reader.getFloat32(start + 4, true)
-      normals[offset + 2] = reader.getFloat32(start + 8, true)
-      offset += 3
-    }
-  }
-  return {positions, normals}
 }
