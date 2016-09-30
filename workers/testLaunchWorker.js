37b7b4e4be2d99657ee68d3ce816e8b9902309ea
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-09-30 11:18:56 +0200

    refactor(workerstream): some cleanups & refinements

--- workers/testLaunchWorker.js
+++ workers/testLaunchWorker.js
@@ -44,37 +44,37 @@ class WorkerStream2 extends Duplex {
       : path
 
     this.bufferedData = undefined
-    this.requestedDataSize = 0
+    this.requestedDataQueue = []
+
     this.worker.onmessage = (e) => {
-      //console.log('data',e)
-      //this.push(Buffer(e.data)) //actual emits
       const data = Buffer(e.data)
 
       this.bufferedData = this.bufferedData ? Buffer.concat([this.bufferedData, data]) : data
 
-      if(this.requestedDataSize > 0){
-        const reqChunk = this.bufferedData.slice(0, this.requestedDataSize)
-        this.push(reqChunk)
-        this.bufferedData = this.bufferedData.slice(this.requestedDataSize)
+      if(this.requestedDataQueue.length > 0){
+        const size = this.requestedDataQueue[0]
+        const reqChunk = this.bufferedData.slice(0, size)
+        this.push(reqChunk)//actual emits requested chunk size
+
+        this.bufferedData = this.bufferedData.slice(size)
+        this.requestedDataQueue.shift()
       }
     }
 
     this.worker.onerror = (err) => {
       this.emit('error', err)
-      console.log('data',err)
     }
   }
 
   _write(chunk, encoding, callback) {
-    console.log('_write',chunk.toString('utf8'),'to worker')
+    //console.log('_write',chunk.toString('utf8'),'to worker')
     this.worker.postMessage(chunk.buffer, [chunk.buffer])
     callback()
   }
 
   _read(size) {
-    console.log('consumer asking read', size)
-    //this.push('foo')
-    this.requestedDataSize = size
+    //console.log('consumer asking read', size)
+    this.requestedDataQueue.push(size)
   }
 }
 
