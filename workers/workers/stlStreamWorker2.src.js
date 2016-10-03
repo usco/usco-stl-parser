02f9bca52be2f2e3d32d8516fe654e449b9d3937
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-10-03 14:05:15 +0200

    feat(stl stream worker): added potential code change for non Buffer based parser

--- workers/workers/stlStreamWorker2.src.js
+++ workers/workers/stlStreamWorker2.src.js
@@ -2,5 +2,6 @@ import { default as makeStlStreamParser } from '../../parsers/stl/parseStreamAlt
 const stlStreamParser = makeStlStreamParser()
 
 self.onmessage = function (event) {
+  //stlStreamParser(event.data, null, (err, data) => self.postMessage(data.buffer, [data.buffer]))
   stlStreamParser(Buffer(event.data), null, (err, data) => self.postMessage(data.buffer, [data.buffer]))
 }
