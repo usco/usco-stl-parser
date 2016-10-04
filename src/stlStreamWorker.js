3a0da7683442320055d1a8c120466557dab798dd
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-10-04 21:12:13 +0200

    chore(workerParser): rebuild of worker after last changes

--- src/stlStreamWorker.js
+++ src/stlStreamWorker.js
@@ -1794,31 +1794,44 @@ module.exports = isArray || function (val) {
 };
 
 },{}],5:[function(require,module,exports){
-(function (Buffer){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = makeStlStreamParser;
+exports.default = makeStreamParser;
 exports.parseASCIIChunk = parseASCIIChunk;
 
 var _utils = require('./utils');
 
-function makeStlStreamParser() {
+/*
+this version of the parser tries to get rid of the dependency on node.js Buffer
+- works for binary files
+- for some reason fails with ASCII files, very likely due to how data is cut and reassembled
+*/
+function makeStreamParser() {
   var chunkNb = 0;
-  var totalSize = 0;
   var isBinary = false;
 
   var faceOffset = 0;
   var previousRemainderData = undefined;
 
   var parser = function parser(chunk, enc, callback) {
-    //console.log('chunk', chunk.length, chunkNb)
+    // console.log('chunk', chunk.length, chunkNb)
     if (chunkNb === 0) {
-      isBinary = (0, _utils.isDataBinaryRobust)(chunk.buffer);
+      isBinary = (0, _utils.isDataBinaryRobust)(chunk);
     }
-    var workChunk = (0, _utils.ensureBinary)(previousRemainderData ? Buffer.concat([previousRemainderData, chunk], previousRemainderData.length + chunk.length) : chunk);
+
+    var workChunk = undefined;
+    if (previousRemainderData) {
+      workChunk = new Uint8Array(previousRemainderData.byteLength + chunk.byteLength);
+      workChunk.set(new Uint8Array(previousRemainderData));
+      workChunk.set(new Uint8Array(chunk), previousRemainderData.byteLength);
+      workChunk = workChunk.buffer;
+      //let data = ensureString(workChunk)
+    } else {
+        workChunk = chunk;
+      }
 
     var _ref = isBinary ? computeBinaryOffset(workChunk, chunkNb) : computASCIIOffset(workChunk, chunkNb);
 
@@ -1830,35 +1843,19 @@ function makeStlStreamParser() {
 
     // update size
     chunkNb += 1;
-    totalSize += chunk.length;
 
     previousRemainderData = parsed.remainderData;
     faceOffset = parsed.faceOffset;
 
-    // we can only send a single buffer out , so concat the two
-    var positionsBuffer = toBuffer(parsed.positions);
-    var normalsBuffer = toBuffer(parsed.normals);
-    // console.log('positions', positionsBuffer.length, parsed.positions.length)
-    //console.log('done with chunk', positionsBuffer, callback)
-    callback(null, Buffer.concat([positionsBuffer, normalsBuffer], positionsBuffer.length + normalsBuffer.length));
+    callback(null, parsed.data);
   };
 
   return parser;
 }
 
-// helper function, taken from https://github.com/feross/typedarray-to-buffer
-function toBuffer(arr) {
-  var buf = new Buffer(arr.buffer);
-  if (arr.byteLength !== arr.buffer.byteLength) {
-    // Respect the "view", i.e. byteOffset and byteLength, without doing a copy
-    buf = buf.slice(arr.byteOffset, arr.byteOffset + arr.byteLength);
-  }
-  return buf;
-}
-
 function computeBinaryOffset(workChunk, chunkNb) {
   var dataStartOffset = 84;
-  var remainingDataInChunk = chunkNb === 0 ? workChunk.length - dataStartOffset : workChunk.length;
+  var remainingDataInChunk = chunkNb === 0 ? workChunk.byteLength - dataStartOffset : workChunk.byteLength;
   var startOffset = chunkNb === 0 ? dataStartOffset : 0;
 
   return { remainingDataInChunk: remainingDataInChunk, startOffset: startOffset };
@@ -1874,12 +1871,13 @@ function parseBinaryChunk(faceOffset, remainingDataInChunk, startOffset, workChu
   var faceLength = 12 * 4 + 2;
   var facesInChunk = Math.floor(remainingDataInChunk / faceLength);
 
-  var reader = new DataView(workChunk.buffer);
+  var reader = new DataView(workChunk);
   var faces = reader.getUint32(80, true);
-  var positions = new Float32Array(facesInChunk * 3 * 3);
-  var normals = new Float32Array(facesInChunk * 3 * 3);
+  // let positions = new Float32Array(facesInChunk * 3 * 3)
+  // let normals = new Float32Array(facesInChunk * 3 * 3)
 
-  var data = new Float32Array(facesInChunk * 2 * 3 * 3); //we want one big typedArray for all the data , to avoid concat operations
+  var halfLength = facesInChunk * 3 * 3;
+  var data = new Float32Array(facesInChunk * 2 * 3 * 3); // we want one big typedArray for all the data , to avoid concat operations
 
   var lface = 0;
   var offset = 0;
@@ -1890,22 +1888,39 @@ function parseBinaryChunk(faceOffset, remainingDataInChunk, startOffset, workChu
     var start = startOffset + lface * faceLength;
     for (var i = 1; i <= 3; i++) {
       var vertexstart = start + i * 12;
-      positions[offset] = reader.getFloat32(vertexstart, true);
-      positions[offset + 1] = reader.getFloat32(vertexstart + 4, true);
-      positions[offset + 2] = reader.getFloat32(vertexstart + 8, true);
+      data[offset] = reader.getFloat32(vertexstart, true);
+      data[offset + 1] = reader.getFloat32(vertexstart + 4, true);
+      data[offset + 2] = reader.getFloat32(vertexstart + 8, true);
 
-      normals[offset] = reader.getFloat32(start, true);
-      normals[offset + 1] = reader.getFloat32(start + 4, true);
-      normals[offset + 2] = reader.getFloat32(start + 8, true);
+      data[offset + halfLength] = reader.getFloat32(start, true);
+      data[offset + halfLength + 1] = reader.getFloat32(start + 4, true);
+      data[offset + halfLength + 2] = reader.getFloat32(start + 8, true);
       offset += 3;
     }
     lface += 1;
   }
 
   // compute offsets, remainderData etc for next chunk etc
-  var remainderData = workChunk.slice(workChunk.length - remainingDataInChunk % faceLength); // chunk length - remainderDataLength
+  var remainderData = workChunk.slice(workChunk.byteLength - remainingDataInChunk % faceLength);
+  // workChunk.slice(workChunk.length - remainingDataInChunk % faceLength) // chunk length - remainderDataLength
 
-  return { faceOffset: lface, remainderData: remainderData, positions: positions, normals: normals };
+  return { faceOffset: lface, remainderData: remainderData, data: data };
+}
+
+// taken from http://stackoverflow.com/questions/6965107/converting-between-strings-and-arraybuffers
+function str2ab(str) {
+  var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
+  var bufView = new Uint16Array(buf);
+  for (var i = 0, strLen = str.length; i < strLen; i++) {
+    bufView[i] = str.charCodeAt(i);
+  }
+  return new Uint8Array(buf);
+  /*var idx, len = str.length, arr = new Array( len );
+   for ( idx = 0 ; idx < len ; ++idx ) {
+       arr[ idx ] = str.charCodeAt(idx) & 0xFF;
+   }
+   // You may create an ArrayBuffer from a standard array (of values) as follows:
+   return new Uint8Array( arr )*/
 }
 
 // ASCII stl parsing
@@ -1926,7 +1941,6 @@ function parseASCIIChunk(workChunk) {
 
   while ((result = patternFace.exec(data)) !== null) {
     var length = 0;
-
     text = result[0];
     offset = result.index + text.length;
 
@@ -1943,21 +1957,23 @@ function parseASCIIChunk(workChunk) {
     faces += 1;
   }
 
-  var positions = new Float32Array(faces * 3 * 3);
-  var normals = new Float32Array(faces * 3 * 3);
-
-  positions.set(posArray);
-  normals.set(normArray);
+  /*let positions = new Float32Array(faces * 3 * 3)
+  let normals = new Float32Array(faces * 3 * 3)
+   positions.set(posArray)
+  normals.set(normArray)*/
+  var outData = new Float32Array(faces * 3 * 3 * 2);
+  outData.set(posArray);
+  outData.set(normArray, posArray.byteLength);
 
   // compute offsets, remainderData etc for next chunk etc
   var remainderTextData = data.slice(offset);
-  // console.log('remainderData', remainderTextData)
-  var remainderData = new Buffer(remainderTextData);
-  return { faceOffset: faces, remainderData: remainderData, positions: positions, normals: normals };
+  var remainderData = str2ab(remainderTextData);
+  var check = (0, _utils.ensureString)(remainderData);
+  //console.log('remainderData', remainderTextData, remainderData)
+  return { faceOffset: faces, remainderData: remainderData, data: outData };
 }
 
-}).call(this,require("buffer").Buffer)
-},{"./utils":7,"buffer":1}],6:[function(require,module,exports){
+},{"./utils":7}],6:[function(require,module,exports){
 (function (Buffer){
 'use strict';
 
