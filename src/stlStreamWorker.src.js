98eabadd7e14457464f9b926bd165dd5b426e911
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-10-06 20:45:20 +0200

    fix(webworker): attempt at fixing web worker useage with webworkify

--- src/stlStreamWorker.src.js
+++ src/stlStreamWorker.src.js
@@ -1,7 +1,10 @@
 import makeStlStreamParser from './parseStream'
-const stlStreamParser = makeStlStreamParser()
 
-self.onmessage = function (event) {
-  // stlStreamParser(event.data, null, (err, data) => self.postMessage(data.buffer, [data.buffer]))
-  stlStreamParser(Buffer(event.data), null, (err, data) => self.postMessage(data.buffer, [data.buffer]))
+module.exports = function (self) {
+  const stlStreamParser = makeStlStreamParser()
+
+  self.onmessage = function (event) {
+    // stlStreamParser(event.data, null, (err, data) => self.postMessage(data.buffer, [data.buffer]))
+    stlStreamParser(Buffer(event.data), null, (err, data) => self.postMessage(data.buffer, [data.buffer]))
+  }
 }
--- src/workerSpawner.js
+++ src/workerSpawner.js
@@ -1,8 +1,9 @@
 import WorkerStream from './WorkerStream'
-
+import WebWorkify from 'WebWorkify'
+//import foo from './foo'
 export default function workerSpawner () {
-  // const worker = WebWorkify(require('.stlStreamWorker.src.js'))
-  const worker = './stlStreamWorker.js'
+  //const worker = new Worker('./stlStreamWorker.src.js')
+  const worker = WebWorkify(require('./stlStreamWorker.src.js'))
   const ws = new WorkerStream(worker)
   return ws
 }
