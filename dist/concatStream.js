a499d6f1e7ddafad1f84c26365558aeb6b1f388b
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-12-02 10:06:28 +0100

    chore(build): uddated build

--- dist/concatStream.js
+++ dist/concatStream.js
@@ -15,7 +15,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var Duplex = require('stream').Duplex;
-//stream-combiner2
+// stream-combiner2
 // a lot of this is taken from https://github.com/maxogden/concat-stream, with gratitude!
 
 function isArrayish(arr) {
@@ -70,6 +70,10 @@ var Formatter = function (_Duplex) {
 
   function Formatter(processorFn) {
     _classCallCheck(this, Formatter);
+
+    processorFn = processorFn || function (data) {
+      return data;
+    };
 
     var _this = _possibleConstructorReturn(this, (Formatter.__proto__ || Object.getPrototypeOf(Formatter)).call(this, { readableObjectMode: true }));
 
--- dist/index.js
+++ dist/index.js
@@ -31,18 +31,37 @@ var _through = require('through2');
 
 var _through2 = _interopRequireDefault(_through);
 
+var _combining = require('combining');
+
+var _combining2 = _interopRequireDefault(_combining);
+
+var _concatStream2 = _interopRequireDefault(_concatStream);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+/**
+ * parses and return a stream of parsed stl data
+ * @param {Object} parameters parameters for the parser
+ * @param {Boolean} parameters.useWorker use web workers (browser only) defaults to true in browser
+ * @param {Boolean} parameters.concat when set to true, stream outputs a single value with all combined data
+ * @return {Object} stream of parsed stl data in the form {positions:TypedArray, normals:TypedArray}
+ */
 function makeStlStream() {
   var parameters = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
   var defaults = {
-    useWorker: _compositeDetect2.default.isBrowser === true
+    useWorker: _compositeDetect2.default.isBrowser === true,
+    concat: true
   };
   parameters = Object.assign({}, defaults, parameters);
   var _parameters = parameters,
-      useWorker = _parameters.useWorker;
+      useWorker = _parameters.useWorker,
+      concat = _parameters.concat;
 
 
-  return useWorker ? (0, _workerSpawner2.default)() : (0, _through2.default)((0, _parseStream2.default)());
+  var mainStream = useWorker ? (0, _workerSpawner2.default)() : (0, _through2.default)((0, _parseStream2.default)());
+  // concatenate result into a single one if needed (still streaming)
+  var endStream = concat ? (0, _combining2.default)()(mainStream, (0, _concatStream2.default)()) : mainStream;
+
+  return endStream;
 }
