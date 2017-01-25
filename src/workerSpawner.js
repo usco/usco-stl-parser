80b2933a0c9da425b94aafe7b086831f851d3f08
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-01-25 14:45:28 +0100

    refactor(cleanup): renamed some files for clarity, minor fixes

--- src/workerSpawner.js
+++ src/workerSpawner.js
@@ -3,7 +3,7 @@ import WebWorkify from 'webWorkify'
 
 export default function workerSpawner () {
   //const worker = new Worker('./stlStreamWorker.src.js')
-  const worker = WebWorkify(require('./stlStreamWorker.js'))
+  const worker = WebWorkify(require('./worker.js'))
   const ws = new WorkerStream(worker)
   return ws
 }
