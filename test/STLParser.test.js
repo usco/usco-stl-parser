70806acddeef708170f3c6f3d6a1acc1a5d5dce5
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-01-25 14:46:30 +0100

    tests(): fixed tests paths

--- test/STLParser.test.js
+++ test/STLParser.test.js
@@ -8,10 +8,10 @@ test.cb('STL parser tests: can parse ascii stl files', t => {
   // this.timeout(5000)
   fs.createReadStream('./data/slotted_disk_ascii.stl', { encoding: null, highWaterMark: 512 * 1024 }) // 'binary'
     .pipe(makeStlStream({concat: true}))
-    .on('data', function (parsedSTL) {
-      t.deepEqual(parsedSTL.positions.length / 3, 864) // we divide by three because each entry is 3 long
-      t.deepEqual(parsedSTL.positions[0], -0.025066649541258812)
-      t.deepEqual(parsedSTL.positions[parsedSTL.positions.length - 1], 0.019999999552965164)
+    .on('data', function (parsed) {
+      t.deepEqual(parsed.positions.length / 3, 864) // we divide by three because each entry is 3 long
+      t.deepEqual(parsed.positions[0], -0.025066649541258812)
+      t.deepEqual(parsed.positions[parsed.positions.length - 1], 0.019999999552965164)
       t.end()
     })
 })
@@ -19,10 +19,10 @@ test.cb('STL parser tests: can parse ascii stl files', t => {
 test.cb('STL parser tests: can parse binary stl files', t => {
   fs.createReadStream('./data/pr2_head_pan_bin.stl')
     .pipe(makeStlStream({concat: true})) // we get a stream back, passing the concat: true option to get the final result only
-    .on('data', function (parsedSTL) {
-      t.deepEqual(parsedSTL.positions.length / 3, 3000) // we divide by three because each entry is 3 long
-      t.deepEqual(parsedSTL.positions[0], -0.07563293725252151)
-      t.deepEqual(parsedSTL.positions[parsedSTL.positions.length - 1], 0.07198309153318405)
+    .on('data', function (parsed) {
+      t.deepEqual(parsed.positions.length / 3, 3000) // we divide by three because each entry is 3 long
+      t.deepEqual(parsed.positions[0], -0.07563293725252151)
+      t.deepEqual(parsed.positions[parsed.positions.length - 1], 0.07198309153318405)
       t.end()
     })
 })
