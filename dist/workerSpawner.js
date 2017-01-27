cbd4af980a1cb2c663845c8ac3ad400d5de293f8
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-01-27 08:19:53 +0100

    chore(build): updated build

--- dist/workerSpawner.js
+++ dist/workerSpawner.js
@@ -9,15 +9,15 @@ var _WorkerStream = require('./WorkerStream');
 
 var _WorkerStream2 = _interopRequireDefault(_WorkerStream);
 
-var _webWorkify = require('webWorkify');
+var _webworkify = require('webworkify');
 
-var _webWorkify2 = _interopRequireDefault(_webWorkify);
+var _webworkify2 = _interopRequireDefault(_webworkify);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function workerSpawner() {
   //const worker = new Worker('./stlStreamWorker.src.js')
-  var worker = (0, _webWorkify2.default)(require('./worker.js'));
+  var worker = (0, _webworkify2.default)(require('./worker.js'));
   var ws = new _WorkerStream2.default(worker);
   return ws;
 }
