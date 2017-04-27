d4f55641b879e3d49b4a617a962c5dc2bfa22e50
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-04-27 16:38:27 +0200

    test(ava): updated ava version, commands  & adjusted tests

--- test/STLParser.test.js
+++ test/STLParser.test.js
@@ -1,11 +1,11 @@
 import test from 'ava'
 const fs = require('fs')
+const path = require('path')
 
 import makeParsedStream from '../src/index'
 
 test.cb('STL parser tests: can parse ascii stl files', t => {
-  // this.timeout(5000)
-  fs.createReadStream('./data/slotted_disk_ascii.stl', { encoding: null, highWaterMark: 512 * 1024 }) // 'binary'
+  fs.createReadStream(path.resolve(__dirname, 'data/slotted_disk_ascii.stl'), { encoding: null, highWaterMark: 512 * 1024 }) // 'binary'
     .pipe(makeParsedStream({concat: true}))
     .on('data', function (parsed) {
       t.deepEqual(parsed.positions.length / 3, 864) // we divide by three because each entry is 3 long
@@ -16,7 +16,7 @@ test.cb('STL parser tests: can parse ascii stl files', t => {
 })
 
 test.cb('STL parser tests: can parse binary stl files', t => {
-  fs.createReadStream('./data/pr2_head_pan_bin.stl')
+  fs.createReadStream(path.resolve(__dirname, 'data/pr2_head_pan_bin.stl'))
     .pipe(makeParsedStream({concat: true})) // we get a stream back, passing the concat: true option to get the final result only
     .on('data', function (parsed) {
       t.deepEqual(parsed.positions.length / 3, 3000) // we divide by three because each entry is 3 long
@@ -26,7 +26,7 @@ test.cb('STL parser tests: can parse binary stl files', t => {
     })
 })
 
-/*test.cb('STL parser tests: should handle errors gracefully', t => {
+/* test.cb('STL parser tests: should handle errors gracefully', t => {
   let data = {foo: '42'}
   let stlObs = parse(data) // we get an observable back
 
@@ -34,4 +34,4 @@ test.cb('STL parser tests: can parse binary stl files', t => {
     t.deepEqual(error.message, 'First argument to DataView constructor must be an ArrayBuffer')
     t.end()
   })
-})*/
+}) */
